338774c9e4f83b5c984d0fa218b533570e448f99
Mamun2k22 <mamun180037@gmail.com>
2026-02-20 03:32:12 +0600

    dd

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,7 +3,7 @@ export default function Section({ title, subtitle, children }) {
     <section className="py-14">
       <div className="mx-auto px-4 mt-2 md:mt-10">
         {title && (
-          <div className="max-w-2xl md:ml-24 ml-2">
+          <div className="max-w-2xl md:ml-4 ml-2">
             <h2 className="text-2xl md:text-3xl font-bold">{title}</h2>
             {subtitle && <p className="mt-2 text-slate-600">{subtitle}</p>}
           </div>
